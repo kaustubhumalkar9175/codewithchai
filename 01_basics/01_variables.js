5a72655fc84ca6652d0da0e2bb851455563e4a30
Kaustubh Rajeev Umalkar <89681510+kaustubhumalkar9175@users.noreply.github.com>
2024-09-12 13:14:09 +0000

    variable in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "kaustubh@gmail.com";
+var accountPassword = "12345";
+accountCity = "Pune";
+let accountState;
+
+// accountId = 2; not allowed
+accountEmail="hc@hc.com"
+accountPassword="21212";
+accountCity="Mumbai"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
