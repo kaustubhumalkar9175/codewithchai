385a0858b90a72d4eef42c07e46c15bb3ccca27d
Kaustubh Rajeev Umalkar <89681510+kaustubhumalkar9175@users.noreply.github.com>
2024-09-13 11:02:59 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert( 3 + 3) // we are using nodejs not browser
+console.log(3 +3); //code readability should be high
+console.log("Kaustubh");
+let name = "Kaustubh";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbols => unique
+
+// object
+
+console.log(typeof );
+
