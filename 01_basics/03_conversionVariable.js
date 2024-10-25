399a687fdf668e911878c87833fd75f7e8470a59
Kaustubh Rajeev Umalkar <89681510+kaustubhumalkar9175@users.noreply.github.com>
2024-10-25 07:33:17 +0000

    conversion of data types

--- 01_basics/03_conversionVariable.js
+++ 01_basics/03_conversionVariable.js
@@ -1,32 +1,29 @@
-let score = null;
+let score = undefined;
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
 
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
-
-
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
-// "33abc"  => NaN
-// true => 1
-// flase => 0
+// "33abc" => NaN
+// true => 1, false => 0
+let isLoggedIn = "Kaustubh";
 
-
-let isLoggedIn = "";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-//console.log(booleanIsLoggedIn);
 
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false;
+// 1 => true; 0=> false
 // "" => false
-// "kaustubh" => true
+// "Kaustubh"=>true
+
 let someNumber = 33;
 let stringNumber = String(someNumber);
-
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
